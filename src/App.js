8a0e36fc7ccc626e3d27386054a036ab6c8deab1
ketan-nimje-plutus <122877145+ketan-nimje-plutus@users.noreply.github.com>
2023-07-07 18:00:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-Hello World        </a>
+        >Hello World</a>
       </header>
     </div>
   );
